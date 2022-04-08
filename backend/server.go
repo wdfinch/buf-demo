e5e8e57a53bcde05424ea18afea68cf9412369f0
wdfinch <william.finch121@gmail.com>
2022-04-08 16:52:44 -0700

    continue backend

--- backend/server.go
+++ backend/server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"go.buf.build/grpc/go/petland/dogapis/petland/dog/v1"
 	"google.golang.org/grpc"
@@ -12,11 +12,7 @@ import (
 )
 
 var (
-	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	certFile   = flag.String("cert_file", "", "The TLS cert file")
-	keyFile    = flag.String("key_file", "", "The TLS key file")
-	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
-	port       = flag.Int("port", 50051, "The server port")
+	port = 50052
 )
 
 type Database struct {
@@ -34,25 +30,30 @@ type Database struct {
 	FavoriteFoodsWithPrice map[string]int `json:"favoriteFoodsWithPrice"`
 }
 
+type Server struct {
+	dogv1.UnimplementedDogApiServiceServer
+}
+
+func (cs *Server) GetDog(ctx context.Context, req *dogv1.GetDogRequest) (*dogv1.GetDogResponse, error) {
+	cRes := &dogv1.GetDogResponse{}
+	fmt.Println("hello")
+	return cRes, nil
+}
+
 func main() {
 	file, _ := ioutil.ReadFile("database.json")
 	database := Database{}
 	_ = json.Unmarshal(file, &database)
 
-	//fmt.Printf("%+v\n", database)
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s%d", ":", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	var opts []grpc.ServerOption
-	s := grpc.NewServer(opts...)
-	log.Println(dogv1.Breed_BREED_BULLDOG)
-	//dServer := &calculatorServer{}
-	//dog.RegisterDogServer(s, dServer)
+	s := grpc.NewServer()
+	dogv1.RegisterDogApiServiceServer(s, &Server{})
 
-	fmt.Println("starting server on port", *port)
+	fmt.Println("starting server on port", port)
 	err = s.Serve(lis)
 	if err != nil {
 		log.Fatal("failed to start server", err)
